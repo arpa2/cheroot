ab16b21ac4cdb5fe945040fdb6f3ece4dd912520
Rick van Rein <rick@openfortress.nl>
2018-10-24 14:00:46 +0000

    Added the 4 parameters to __init__ from the Adapter base class. This also defines the usage pattern by tools like CherryPy.

--- cheroot/ssl/tlspooladapter.py
+++ cheroot/ssl/tlspooladapter.py
@@ -1,4 +1,42 @@
-"""Implementation of the SSL Adapter for the TLS Pool."""
+"""Implementation of the SSL Adapter for the TLS Pool.
+
+When you use cheroot directly, you can specify an
+`ssl_adapter` set to an instance of this class.
+Using the WSGI standard example, you might adapt
+it thusly:
+
+    from cheroot import wsgi
+    from cheroot.ssl.tlspooladapter import TLSPoolAdapter
+    
+    def my_crazy_app(environ, start_response):
+        status = '200 OK'
+        response_headers = [('Content-type','text/plain')]
+        start_response(status, response_headers)
+        return [b'Hello world!\r\nThis is the TLS Pool variant of cheroot\r\n']
+    
+    addr = '0.0.0.0', 8070
+    server = wsgi.Server(addr, my_crazy_app, server_name='tlspool.arpa2.lab')
+    server.ssl_adapter = TLSPoolAdapter ('tlspool.arpa2.lab')
+    server.start()
+
+In comparison to the standard WSGI server, you have added
+one `import` line and set the `server.ssl_adaptor` to an
+instance of the plugin class defined in this module.
+
+The idea of the TLS Pool is to isolate long-term credentials
+from application code.  This allows code to be more freely
+developed, and an occasional security breach to never get to
+the crown jewels of the site, thanks to the strict separation
+of processes maintained by the operating system.  It also
+allows central management of credentials, which is a nuisance
+up to a point of a certain scale, where it becomes divine.
+The intention of the InternetWide Architecture and specifically
+the IdentityHub is to automate so much of the flow surrounding
+the TLS Pool that this point of divinity is at zero.  More on
+http://internetwide.org -- yes, insecure as it is just a blog,
+so the burden of management is avoided until our tools make
+it a breeze!
+"""
 
 
 from . import Adapter
@@ -32,8 +70,25 @@ class TLSPoolAdapter (Adapter):
         """Initialise this object and ignore the customary
            things: cert, key, chain, ciphers are all handled
            by the TLS Pool, so we can be blissfully ignorant.
+
+           This __init__() function is not the usual one
+           being called; instead, the Adapter base class
+           promises a function with four arguments that
+           is normally called by the environment.
         """
         self.server_name = server_name
+
+    def __init__ (self, server_name, *mooh):
+        """The other plugins in this directory expect 4 args,
+           namely cert, key, chain, ciphers.  This information
+           is moot (or mooh) to this Adapter because management
+           issues like that are centralised to the TLS Pool.
+           You are however permitted to provide all four args,
+           where the first (usually the certificate path) is
+           interpreted as the server name.  The TLS Pool will
+           look for a certificate to go with that.
+        """
+        self.__init__ (server_name)
 
     def bind (self, sock):
         """Wrap and return the socket.
